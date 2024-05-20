a5d5e4278db642dc0e68555a0bc87ad5eb95b44f
unknown <darodriguesufc@gmail.com>
2024-05-20 16:01:41 -0300

    adicionado proxy

--- backend/sistema/whatsapp/functions/DefaultConnectionConfigs.ts
+++ backend/sistema/whatsapp/functions/DefaultConnectionConfigs.ts
@@ -3,7 +3,7 @@ dotenv.config();
 import fetch from 'node-fetch'
 const WPPAPI_URL = (process.env.PROD_ENV==='TRUE')?(`https://${process.env.WPPAPI_PROXY}`):(`http://localhost:${process.env.WPPAPI_PORT}`);
 
-export const setDefaultConnectionConfig = async(user_name:string, connection_name:string)=>{
+export const setDefaultConnectionConfig = async(user_name:string, connection_name:string):Promise<any>=>{
     const server_connection_name = user_name?.toString() + '_' + (connection_name?.replace(/\s+/g, ' ')?.trim())?.toString();
   
     const headers0 = {
@@ -30,8 +30,33 @@ export const setDefaultConnectionConfig = async(user_name:string, connection_nam
         });
         console.log('SAIDA DEFAULT CONFIG')
         console.log(await response.json())
-        return true;
       }catch(error){
         console.log(error)
       }
+
+    if(process.env.WILL_PROXY==='TRUE'){
+      try{
+        const default_conf = {
+            "enabled": true,
+            "proxy": {
+              "host":process.env.PROXY_HOST,
+              "port":process.env.PROXY_PORT,
+              "protocol":"http",
+              "username":process.env.PROXY_USERNAME,
+              "password":process.env.PROXY_PASSWORD
+            }
+          }
+  
+        const response = await fetch(`${WPPAPI_URL}/proxy/set/${server_connection_name}`, {
+          method: 'POST',
+          headers: headers0,
+          body: JSON.stringify(default_conf)
+        });
+        console.log('SAIDA DEFAULT CONFIG')
+        console.log(await response.json())
+      }catch(error){
+        console.log(error)
+      }
+      return true;
+    }
 }
